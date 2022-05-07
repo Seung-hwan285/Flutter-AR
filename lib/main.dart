d690236548e2cbe9b62a4a3e001e023804b010ea
zzangse1 <zzangse1014@gmail.com>
2022-05-07 19:02:58 +0900

    로그인기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ar/login_page.dart';
+import 'package:get/get.dart';
+
 //하 제발 
 void main(){
 
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return new GetMaterialApp(
 
       title: "hello",
       theme: ThemeData(
